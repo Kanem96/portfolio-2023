1315a828f83374baf42423211c172741f8596f66
Kane Milligan <kane.milligan@glean.co>
2023-10-26 00:22:25 +0100

    made project cards rounder

--- components/project.tsx
+++ components/project.tsx
@@ -27,7 +27,7 @@ const Project: FunctionComponent<ProjectProps> = ({
         scale: scaleProgress,
         opacity: opacityProgress,
       }}
-      className="group bg-slate-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-slate-200 transition"
+      className="group bg-slate-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-slate-200 transition rounded-lg"
     >
       <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[26rem]">
         <h3 className="text-2xl font-semibold">{title}</h3>
