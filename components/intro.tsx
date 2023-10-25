08f56f88982027068d5ea5e31fda4cf4b17ca5be
Kane Milligan <kane.milligan@glean.co>
2023-10-25 19:50:38 +0100

    added social links etc

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { HiDownload } from "react-icons/hi";
 
 const Intro = () => {
   return (
-    <section id="intro">
+    <section id="intro" className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <Image
@@ -13,10 +17,53 @@ const Intro = () => {
             height="300"
             quality="95"
             priority={true}
-            className="w-[120px] h-[120px] object-cover rounded-full ring ring-white ring-offset-2 shadow-xl"
+            className="w-24 h-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
           />
           <span className="absolute bottom-0 right-0 text-4xl">👋🏻</span>
         </div>
+      </div>
+
+      <h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
+        <span className="font-bold">Hey, I'm Kane.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">2 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My current focus
+        is on building <span className="underline">React apps</span>.
+      </h1>
+
+      <div className="flex flex-col justify-center items-center sm:flex-row gap-2 px-4 text-lg font-medium">
+        <Link
+          href="#contact"
+          className="group bg-slate-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-slate-950 active:scale-105 transition"
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/cv.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/kane-milligan/"
+          target="blank"
+          className="bg-white p-4 text-slate-700 flex items-center gap-2 rounded-full ffocus:scale-[1.15] hover:scale-[1.15] hover:text-slate-950 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          href="https://github.com/Kanem96"
+          target="blank"
+          className="bg-white p-4 text-slate-700 flex items-center gap-2 rounded-full text-[1.32rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-slate-950 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <FaGithubSquare />
+        </a>
       </div>
     </section>
   );
