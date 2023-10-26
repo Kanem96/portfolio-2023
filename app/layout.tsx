6a2add152a01eae052e40efe1865687b511827f2
Kane Milligan <kane.milligan@glean.co>
2023-10-26 19:08:11 +0100

    add active section context

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import ActiveSectionContextProvider from "@/context/active-section-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,8 +24,10 @@ export default function RootLayout({
         <div className="bg-[#e9c0e8] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.23rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#c7dae6] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
-        <Header />
-        {children}
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,14 @@
 "use client";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { links } from "@/lib/data";
 import Link from "next/link";
 import clsx from "clsx";
 import { motion } from "framer-motion";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 const Header = () => {
-  const [activeSection, setActiveSection] = useState("Home");
+  const { activeSection, setActiveSection } = useActiveSectionContext();
+
   return (
     <header className="z-[999] relative">
       <div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white/80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"></div>
