28baeab3a2b8120809797a9cc4b7cec4d92d8590
Kane Milligan <kane.milligan@glean.co>
2023-10-28 21:01:52 +0100

    added darkmode classes to header

--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
 
   return (
     <header className="z-[999] relative">
-      <div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white/80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"></div>
+      <div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white/80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-slate-950 dark:border-black/40 dark:bg-opacity-75"></div>
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-slate-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
@@ -24,9 +24,10 @@ const Header = () => {
             >
               <Link
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-slate-950 transition",
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-slate-950 transition dark:text-slate-400 dark:hover:text-slate-300",
                   {
-                    "text-slate-950": activeSection === link.name,
+                    "text-slate-950 dark:text-slate-300":
+                      activeSection === link.name,
                   }
                 )}
                 href={link.hash}
@@ -38,7 +39,7 @@ const Header = () => {
                 {link.name}
                 {link.name === activeSection && (
                   <motion.span
-                    className="bg-slate-100 rounded-full absolute inset-0 -z-10"
+                    className="bg-slate-100 rounded-full absolute inset-0 -z-10 dark:bg-slate-800"
                     layoutId="activeSection"
                     transition={{
                       type: "spring",
